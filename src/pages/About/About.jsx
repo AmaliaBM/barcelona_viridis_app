55105a5073aa10dbb6fb7e60e2ba99f4de369537
amalia barrigas <amaliabarrigas@gmail.com>
2025-05-28 15:46:51 +0200

    He añadido una fotografía mía, también el link a mi linkedin en la parte de "about"

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,28 +1,29 @@
-
-
 import "./About.css";
+import picture from '../../assets/me.jpg';
 
 function About() {
   return (
     <div id="autor">
-      <h2>Autora:</h2>
+      <div className="autor-nombre">
+        <h2>Autora:</h2>
+        <h3>Amalia Barrigas</h3>
+      </div>
 
       <div>
-        <h3>Amalia Barrigas</h3>
-        <div class="git">
-          <img src="" id=""></img>
-          <p>
-            Github: <a href="https://github.com/AmaliaBM">AmaliaBM</a>
-          </p>
-        </div>
-        <div class="git">
-          <img src="" id=""></img>
-          <p>
-            LinkedIn:{" "}
-            <a href="https://www.linkedin.com/in/amaliabarrigasmunuera/">
-              Amalia Barrigas Munuera
-            </a>
-          </p>
+        <div className="git">
+          <img 
+            src={picture} 
+            alt="Foto de Amalia Barrigas" 
+          />
+          <div>
+            <p>
+              LinkedIn:{" "}
+              <a href="https://www.linkedin.com/in/amaliabarrigasmunuera/">
+                Amalia Barrigas Munuera
+              </a>
+            </p>
+            <p>UX/UI | Ilustrator | Web development | Studying psychology at UNED as a hobby</p>
+          </div>
         </div>
       </div>
     </div>
